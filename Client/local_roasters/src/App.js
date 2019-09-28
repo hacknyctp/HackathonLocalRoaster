a5291b7f2357ecacc29962fe0b3a062e75504a38
Sparkadelic177 <sparkyevangelista@gmail.com>
2019-09-28 14:01:44 -0400

    added rotuer

--- Client/local_roasters/src/App.js
+++ Client/local_roasters/src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
+import SignUp from "./pages/SignUp"
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
+import Signup from './pages/SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" component={Splash} />
+        <Route exact path="/price" component={Price} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/dashboard" component={Dashboard} />
+      </div>
+    </Router>
   );
 }
 
---
+++ Client/local_roasters/src/pages/SignUp.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import "SignUp.css";
+
+
+
+export default class Signup extends Comment {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
