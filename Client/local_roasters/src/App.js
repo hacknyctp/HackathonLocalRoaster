4e0b98d28c55a45ba7c7740d1c61eac6984c1284
ericsei <eric.ahhpusei@gmail.com>
2019-09-28 19:25:58 -0400

    WIP

--- Client/local_roasters/src/App.js
+++ Client/local_roasters/src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import SignUp from "./pages/SignUp"
-import DashBoard from "./pages/DashBoard"
+import SignUp from './pages/SignUp';
+import Price from './pages/Price';
+import Splash from './pages/Splash';
+import DashBoard from './pages/DashBoard';
 import './App.css';
 import Signup from './pages/SignUp';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Route exact path="/" component={DashBoard} />
-        {/* <Route exact path="/price" component={Price} /> */}
-        {/* <Route exact path="/signup" component={Signup} /> */}
-        {/* <Route exact path="/dashboard" component={Dashboard} /> */}
+      <div className='App'>
+        <Route exact path='/' component={Splash} />
+        <Route exact path='/price' component={Price} />
+        <Route exact path='/signup' component={Signup} />
       </div>
     </Router>
   );
--- Client/local_roasters/src/pages/Splash.js
+++ Client/local_roasters/src/pages/Splash.js
@@ -1,61 +1,60 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 const Splash = () => {
-    const [coffee, setCoffeeType] = useState('');
-    const onChange = e => {
-        setCoffeeType(e.target.value);
-        sessionStorage.setItem(e.target.name, e.target.value);
-    };
-    return (
-        <div className='container-fluid'>
-            <div className='jumbotron'>
-                <h3>Local Rosters</h3>
-                <p>We care about your coffee and maximum price...</p>
-            </div>
-            What's your type of coffee ? <br />
-            <form className='form-group'>
-                {/* <div className='form-check'>
-         <input type='radio' value={coffee} onChange={onChange} /> Light <br />
-       </div> */}
-                <div className='form-check'>
-                    <input
-                        className='form-check-input'
-                        type='radio'
-                        name='coffee'
-                        value='light'
-                        onChange={onChange}
-                    />
-                    <label className='form-check-label' for='exampleRadios1'>
-                        Light
-         </label>
-                </div>
-                <div className='form-check'>
-                    <input
-                        className='form-check-input'
-                        type='radio'
-                        name='coffee'
-                        value='medium'
-                        onChange={onChange}
-                    />
-                    <label className='form-check-label' for='exampleRadios1'>
-                        Medium
-         </label>
-                </div>
-                <div className='form-check'>
-                    <input
-                        className='form-check-input'
-                        type='radio'
-                        name='coffee'
-                        value='strong'
-                        onChange={onChange}
-                    />
-                    <label className='form-check-label' for='exampleRadios1'>
-                        Strong
-         </label>
-                </div>
-                <Link to='/price'>Continue</Link>
-            </form>
-        </div>
-    );
+  const [coffee, setCoffeeType] = useState('');
+  const onChange = e => {
+    //setCoffeeType(e.target.value);
+    sessionStorage.setItem(e.target.name, e.target.value);
+    alert('on change');
+  };
+  return (
+    <div className='container-fluid'>
+      <div className='jumbotron'>
+        <h3>Local Rosters</h3>
+        <p>We care about your coffee and maximum price...</p>
+      </div>
+      What's your type of coffee ? <br />
+      <div
+        class='btn-group-vertical'
+        role='group'
+        aria-label='Basic example'
+        name='coffee'
+        value='light'
+      >
+        <button
+          type='button'
+          class='btn btn-primary'
+          type='button'
+          onClick={onChange}
+        >
+          Light
+        </button>
+        <br />
+        <button type='button' class='btn btn-primary'>
+          Medium
+        </button>
+        <br />
+        <button type='button' class='btn btn-primary'>
+          Strong
+        </button>
+      </div>
+      {/* <div className='btn btn-secondary'>
+          <input
+            className='form-check-input'
+            type='button'
+            name='coffee'
+            value='strong'
+            onChange={onChange}
+          />
+          <label className='form-check-label' for='exampleRadios1'>
+            Strong
+          </label> 
+
+  </div>*/}
+      <div>
+        <Link to='/price'>Continue</Link>
+      </div>
+    </div>
+  );
 };
 export default Splash;
