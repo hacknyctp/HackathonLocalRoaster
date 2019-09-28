00a84859204c52bbc6b519c85d5fe90879b25f11
Mark Goldstein <markgoldstein12@gmail.com>
2019-09-28 14:02:10 -0400

    mongo and express

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express')
 const app = express()
+const connectDB = require('./config/db')
+connectDB()//Call and connect to the db
+
+app.use(express.json({ extended: false })); //Now we can accept body data
 
 //Home page test
-app.get('/:id', (req, res) => {
-    res.json({id: req.params.id})
-})
 app.get('/', (req, res) => {
-    res.json({id: "hello"})
+    res.json({msg: "hello from the backend"})
 })
+app.post('/', (req, res) => {
+    res.json({msg: "you made a user"})
+})
+
+
+
+// app.get('/', (req, res) => {
+//     res.json({id: req.params.id})
+// })
+
 
 
 const PORT = process.env.PORT || 5000;
