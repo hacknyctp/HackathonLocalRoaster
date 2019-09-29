5434baab8ec4f1916c20583b6800f2f9696b87cf
Sparkadelic177 <sparkyevangelista@gmail.com>
2019-09-28 23:39:08 -0400

    adding signip

--- Client/local_roasters/src/pages/SignUp.js
+++ Client/local_roasters/src/pages/SignUp.js
@@ -4,127 +4,109 @@ import '../styles/SignUp.css';
 import Navbar from '../components/Navbar';
 
 export default class Signup extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      location: '10033',
-      email: 'sparky@gmail.com',
-      password: 'abc123',
-      retype: 'abc123'
-    };
-  }
-
-  formHandler = e => {
-    console.log(this.state[e.target.id]);
-    this.setState({ [e.target.id]: e.target.value });
-  };
-
-<<<<<<< HEAD
-  submitHandler = () => {
-    sessionStorage.setItem('price', 4);
-    sessionStorage.setItem('coffee', 'black');
-    //this sends the information to the sign up api
-    const { location, email, password, retype } = this.state;
-
-    if (password === retype) {
-      axios
-        .post('https://local-roasters-api.herokuapp.com/users/signup', {
-          email: email,
-          password: password,
-          location: location,
-          coffee: sessionStorage.getItem('coffee'),
-          price: sessionStorage.getItem('price')
-        })
-        .then(res => localStorage.setItem('token', res.data.token))
-        .then(() => this.props.history.push('/dashboard'))
-        .catch(error => console.log(error));
-    } else {
-      alert('The passwords do not match');
+    constructor(props) {
+        super(props);
+        this.state = {
+            location: '10033',
+            email: 'sparky@gmail.com',
+            password: 'abc123',
+            retype: 'abc123'
+        };
     }
-  };
-  render() {
-    return (
-      <div className='container-fluid'>
-        <div className='jumbotron'>
-          <h3>
-            <i className='fas fa-mug-hot'></i> Local Roasters
-          </h3>
-          <hr class='my-3'></hr>
-          <p>Where are you getting your Coffee ? </p>
-        </div>
 
-        <div>
-          <div className='form-group'>
-            <input
-              className='form-control'
-              id='location'
-              type='numeric'
-              value={this.state.location}
-              placeholder='zip code'
-              onChange={event => this.formHandler(event)}
-              required
-            />
-          </div>
-=======
+    formHandler = e => {
+        console.log(this.state[e.target.id]);
+        this.setState({ [e.target.id]: e.target.value });
+    };
+    submitHandler = () => {
+        sessionStorage.setItem('price', 4);
+        sessionStorage.setItem('coffee', 'black');
+        //this sends the information to the sign up api
+        const { location, email, password, retype } = this.state;
+
         if (password === retype) {
-            axios.post("https://local-roasters-api.herokuapp.com/users/signup", {
-                email: email,
-                password: password,
-                location: location,
-                coffee: sessionStorage.getItem('coffee'),
-                price: sessionStorage.getItem('price'),
-            })
+            axios
+                .post('https://local-roasters-api.herokuapp.com/users/signup', {
+                    email: email,
+                    password: password,
+                    location: location,
+                    coffee: sessionStorage.getItem('coffee'),
+                    price: sessionStorage.getItem('price')
+                })
                 .then(res => localStorage.setItem('token', res.data.token))
-                .then(() => this.props.history.push("/dashboard"))
-                .catch(error => console.log(error))
+                .then(() => this.props.history.push('/dashboard'))
+                .catch(error => console.log(error));
         } else {
-            alert("The passwords do not match");
+            alert('The passwords do not match');
         }
->>>>>>> 0423ad1aa5b70a2a87e7ac736e981b9efede9659
+    };
+    render() {
+        return (
+            <div className='container-fluid'>
+                <div className='jumbotron'>
+                    <h3>
+                        <i className='fas fa-mug-hot'></i> Local Roasters
+          </h3>
+                    <hr class='my-3'></hr>
+                    <p>Where are you getting your Coffee ? </p>
+                </div>
 
-          <div className='form-group'>
-            <input
-              className='form-control'
-              id='email'
-              type='text'
-              value={this.state.email}
-              placeholder='email'
-              onChange={event => this.formHandler(event)}
-              required
-            />
-          </div>
-          <div className='form-group'>
-            <input
-              className='form-control'
-              id='password'
-              type='password'
-              value={this.state.password}
-              placeholder='password'
-              onChange={event => this.formHandler(event)}
-              required
-            />
-          </div>
-          <div className='form-group'>
-            <input
-              className='form-control'
-              id='retype'
-              type='password'
-              value={this.state.retype}
-              placeholder='retype password'
-              onChange={event => this.formHandler(event)}
-              required
-            />
-          </div>
-          <div className='form-group'>
-            <input
-              className='btn btn-lg btn-primary'
-              type='submit'
-              value='Continue'
-              onClick={this.submitHandler}
-            />
-          </div>
-        </div>
-      </div>
-    );
-  }
+                <div>
+                    <div className='form-group'>
+                        <input
+                            className='form-control'
+                            id='location'
+                            type='numeric'
+                            value={this.state.location}
+                            placeholder='zip code'
+                            onChange={event => this.formHandler(event)}
+                            required
+                        />
+                    </div>
+
+                    <div className='form-group'>
+                        <input
+                            className='form-control'
+                            id='email'
+                            type='text'
+                            value={this.state.email}
+                            placeholder='email'
+                            onChange={event => this.formHandler(event)}
+                            required
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <input
+                            className='form-control'
+                            id='password'
+                            type='password'
+                            value={this.state.password}
+                            placeholder='password'
+                            onChange={event => this.formHandler(event)}
+                            required
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <input
+                            className='form-control'
+                            id='retype'
+                            type='password'
+                            value={this.state.retype}
+                            placeholder='retype password'
+                            onChange={event => this.formHandler(event)}
+                            required
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <input
+                            className='btn btn-lg btn-primary'
+                            type='submit'
+                            value='Continue'
+                            onClick={this.submitHandler}
+                        />
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
