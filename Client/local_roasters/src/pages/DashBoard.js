d9ea06c12b28f6e6b286a379d86a5aa276805cb6
Mark Goldstein <markgoldstein12@gmail.com>
2019-09-29 09:25:36 -0400

    pref

--- Client/local_roasters/src/pages/DashBoard.js
+++ Client/local_roasters/src/pages/DashBoard.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import Slides from "../components/Slides";
 import { Swipeable } from "react-swipeable";
 import axios from "axios";
@@ -9,136 +10,147 @@ const LEFT = "+1";
 const IMG_WIDTH = "342px";
 const IMG_HEIGHT = "249px";
 
-
 const buttonLeft = { float: "left" };
 const buttonRight = { float: "right" };
 
 export default class DashBoard extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            typeOfCoffee: "",
-            location: "",
-            price: "",
-            recomendations: [{ location: "" }],
-            slideIndex: 0
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      typeOfCoffee: "",
+      location: "",
+      price: "",
+      recomendations: [{ location: "" }],
+      slideIndex: 0
+    };
+  }
+  //this changes slide index
+  onSwiped(direction) {
+    const { recomendations } = this.state;
+    const change = direction === RIGHT ? RIGHT : LEFT;
+    const adjustedIdx = this.state.slideIndex + Number(change);
+    let newIdx;
+    if (adjustedIdx >= recomendations.length) {
+      //after last slide go to first
+      newIdx = 0;
+    } else if (adjustedIdx < 0) {
+      //if swipe left on first go to last
+      newIdx = recomendations.length - 1;
+    } else {
+      //else go to next slide back or forth
+      newIdx = adjustedIdx;
     }
-    //this changes slide index
-    onSwiped(direction) {
-        const { recomendations } = this.state;
-        const change = direction === RIGHT ? RIGHT : LEFT;
-        const adjustedIdx = this.state.slideIndex + Number(change);
-        let newIdx;
-        if (adjustedIdx >= recomendations.length) {
-            //after last slide go to first
-            newIdx = 0;
-        } else if (adjustedIdx < 0) {
-            //if swipe left on first go to last
-            newIdx = recomendations.length - 1;
-        } else {
-            //else go to next slide back or forth
-            newIdx = adjustedIdx;
-        }
-        this.setState({ slideIndex: newIdx });
-    }
+    this.setState({ slideIndex: newIdx });
+  }
 
-    //this is going to get the user data from the database
-    componentDidMount() {
-        console.log(localStorage.getItem("token"));
-        axios("https://local-roasters-api.herokuapp.com/users", {
-            headers: { "x-auth-token": localStorage.getItem("token") }
+  //this is going to get the user data from the database
+  componentDidMount() {
+    console.log(localStorage.getItem("token"));
+    axios("https://local-roasters-api.herokuapp.com/users", {
+      headers: { "x-auth-token": localStorage.getItem("token") }
+    })
+      // .then(res => console.log(res))
+      .then(res =>
+        this.setState({
+          typeOfCoffee: res.data.coffee,
+          price: res.data.price,
+          location: res.data.location,
+          price: res.data.price
         })
-            // .then(res => console.log(res))
-            .then(res =>
-                this.setState({
-                    typeOfCoffee: res.data.coffee,
-                    price: res.data.price,
-                    location: res.data.location,
-                    price: res.data.price
-                })
-            )
-            .catch(error => {
-                console.log(error.message);
-            })
-            .then(() => {
-                //second api call to get the locations
-                axios("https://local-roasters-api.herokuapp.com/roasters/getRoasters", {
-                    headers: { "x-auth-token": localStorage.getItem("token") },
-                    params: {
-                        zipcode: 11214
-                    }
-                })
-                    .then(res => this.setState({ recomendations: [...res.data] }))
-                    .catch(err => {
-                        console.log("error " + err);
-                        window.location.replace("/");
-                    });
-            })
-            .catch(error => {
-                error.error(error.message);
-            });
-    }
+      )
+      .catch(error => {
+        console.log(error.message);
+      })
+      .then(() => {
+        //second api call to get the locations
+        axios("https://local-roasters-api.herokuapp.com/roasters/getRoasters", {
+          headers: { "x-auth-token": localStorage.getItem("token") },
+          params: {
+            zipcode: 11214
+          }
+        })
+          .then(res => this.setState({ recomendations: [...res.data] }))
+          .catch(err => {
+            console.log("error " + err);
+            window.location.replace("/");
+          });
+      })
+      .catch(error => {
+        error.error(error.message);
+      });
+  }
 
-    logout = () => {
-        localStorage.clear();
-        this.props.history.push("/");
-    }
+  logout = () => {
+    localStorage.clear();
+    this.props.history.push("/");
+  };
 
-    render() {
-        const {
+  render() {
+    const {
       typeOfCoffee,
-            location,
-            price,
-            recomendations,
-            slideIndex
-            } = this.state;
-        const currentPlace = recomendations[slideIndex];
-        return (
-            <div>
-
-                <div className='d-flex flex-column align-items-center '>
-                    <div className="container-fluid">
-                        <div className="jumbotron" style={{ marginTop: "-5px" }}>
-                            <img className="logout" src={require("../assets/logout.svg")} onClick={this.logout} />
-                            <br />
-                            <h3>
-                                <i className="fas fa-mug-hot"></i> Local Roasters
-                        </h3>
-                        </div>
-                    </div>
-                </div>
-                <Swipeable
-                    trackMouse
-                    preventDefaultTouchmoveEvent
-                    onSwipedLeft={() => this.onSwiped(LEFT)}
-                    onSwipedRight={() => this.onSwiped(RIGHT)}
-                >
-                    <h2 className="m-5">
-                        {typeOfCoffee.substring(0, 1).toUpperCase()}
-                        {typeOfCoffee.substring(1)} coffee within ${price} near {location}
-                    </h2>
-                    <div
-                        className="d-flex card"
-                        style={{ maxWidth: "100%", position: "abosulte" }}
-                    >
-                        <button onClick={() => this.onSwiped(RIGHT)} className="buttonStyle" style={buttonLeft}>
-                            ⇦
-          </button>
-                        <Slides
-                            price={currentPlace.price}
-                            coffee={currentPlace.coffee}
-                            name={currentPlace.name}
-                            address={currentPlace.location.address}
-                            img={currentPlace.img}
-                            beans={currentPlace.beans}
-                        />
-                        <button onClick={() => this.onSwiped(LEFT)} className="buttonStyle" style={buttonRight}>
-                            ⇨
-          </button>
-                    </div>
-                </Swipeable>
+      location,
+      price,
+      recomendations,
+      slideIndex
+    } = this.state;
+    const currentPlace = recomendations[slideIndex];
+    return (
+      <div>
+        <div className="d-flex flex-column align-items-center ">
+          <div className="container-fluid">
+            <div className="jumbotron" style={{ marginTop: "-5px" }}>
+              <img
+                className="logout"
+                src={require("../assets/logout.svg")}
+                onClick={this.logout}
+              />
+              <br />
+              <Link to="/preferences">preferences</Link>
+              <h3>
+                <i className="fas fa-mug-hot"></i> Local Roasters
+              </h3>
             </div>
-        );
-    }
+          </div>
+        </div>
+        <Swipeable
+          trackMouse
+          preventDefaultTouchmoveEvent
+          onSwipedLeft={() => this.onSwiped(LEFT)}
+          onSwipedRight={() => this.onSwiped(RIGHT)}
+        >
+          <h2 className="m-5">
+            {typeOfCoffee.substring(0, 1).toUpperCase()}
+            {typeOfCoffee.substring(1)} coffee within ${price} near {location}
+          </h2>
+          <div
+            className="d-flex card"
+            style={{ maxWidth: "100%", position: "abosulte" }}
+          >
+            <button
+              onClick={() => this.onSwiped(RIGHT)}
+              className="buttonStyle"
+              style={buttonLeft}
+            >
+              ⇦
+            </button>
+            <Slides
+              price={currentPlace.price}
+              coffee={currentPlace.coffee}
+              name={currentPlace.name}
+              address={currentPlace.location.address}
+              img={currentPlace.img}
+              beans={currentPlace.beans}
+            />
+            <button
+              onClick={() => this.onSwiped(LEFT)}
+              className="buttonStyle"
+              style={buttonRight}
+            >
+              ⇨
+            </button>
+          </div>
+        </Swipeable>
+      </div>
+    );
+  }
 }
--- Client/local_roasters/src/pages/Splash.js
+++ Client/local_roasters/src/pages/Splash.js
@@ -1,70 +1,70 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 const Splash = () => {
-  const [coffee, setCoffeeType] = useState('');
+  const [coffee, setCoffeeType] = useState("");
   const onChange = e => {
     //setPrice(e.target.value);
-    let node = document.getElementsByClassName('coffee-items');
+    let node = document.getElementsByClassName("coffee-items");
 
     for (let i = 0; i < node.length; i++) {
-      node[i].className = 'bbtn btn-lg btn-primary coffee-items';
+      node[i].className = "bbtn btn-lg btn-primary coffee-items";
     }
 
-    e.target.className = 'btn btn-lg btn-success coffee-items';
+    e.target.className = "btn btn-lg btn-success coffee-items";
 
     sessionStorage.setItem(e.target.name, e.target.value);
   };
 
   return (
-    <div className='container-fluid'>
-      <div className='jumbotron'>
+    <div className="container-fluid">
+      <div className="jumbotron">
         <h3>
-          <i className='fas fa-mug-hot'></i> Local Rosters
+          <i className="fas fa-mug-hot"></i> Local Rosters
         </h3>
-        <hr className='my-3'></hr>
+        <hr className="my-3"></hr>
         <p> Your location-based, personalized coffee experience</p>
       </div>
       What Roast of Coffee do You Like?
       <br />
-      <div className='btn-group btn-group-price' role='group'>
-        <div className='btn-group-item'>
+      <div className="btn-group btn-group-price" role="group">
+        <div className="btn-group-item">
           <input
-            className='btn btn-lg btn-primary coffee-items'
-            type='button'
-            name='coffee'
-            value='Light'
+            className="btn btn-lg btn-primary coffee-items"
+            type="button"
+            name="coffee"
+            value="Light"
             onClick={onChange}
           />
         </div>
-        <div className='btn-group-item'>
+        <div className="btn-group-item">
           <input
-            className='btn btn-lg btn-primary coffee-items'
-            type='button'
-            name='coffee'
-            value='Medium'
+            className="btn btn-lg btn-primary coffee-items"
+            type="button"
+            name="coffee"
+            value="Medium"
             onClick={onChange}
           />
         </div>
-        <div className='btn-group-item'>
+        <div className="btn-group-item">
           <input
-            className='btn btn-lg btn-primary coffee-items'
-            type='button'
-            name='coffee'
-            value='Dark'
+            className="btn btn-lg btn-primary coffee-items"
+            type="button"
+            name="coffee"
+            value="Dark"
             onClick={onChange}
           />
         </div>
       </div>
       <br />
-      <div className='btn btn-success'>
-        <Link className='links' to='/price'>
+      <div className="btn btn-success">
+        <Link className="links" to="/price">
           Let's caffeinate
         </Link>
       </div>
       <br />
       <div>
-        <Link className='links' to='/login'>
-          <span className='links-small'>Already Sign Up? |</span> Log In
+        <Link className="links" to="/login">
+          <span className="links-small">Already Sign Up? |</span> Log In
         </Link>
       </div>
     </div>
