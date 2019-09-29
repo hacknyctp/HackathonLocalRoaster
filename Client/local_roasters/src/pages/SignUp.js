bb47aa007760d97fad1da28d03b395407a5ed8d8
Sparkadelic177 <sparkyevangelista@gmail.com>
2019-09-29 00:36:21 -0400

    fixed merge conflict

--- Client/local_roasters/src/pages/SignUp.js
+++ Client/local_roasters/src/pages/SignUp.js
@@ -21,101 +21,102 @@ export default class Signup extends Component {
     submitHandler = () => {
         //this sends the information to the sign up api
         const { location, email, password, retype } = this.state;
+    }
 
+    formHandler = e => {
+        console.log(this.state[e.target.id]);
+        this.setState({ [e.target.id]: e.target.value });
+    };
+    submitHandler = () => {
+        sessionStorage.setItem('price', 4);
+        sessionStorage.setItem('coffee', 'black');
+        //this sends the information to the sign up api
+        const { location, email, password, retype } = this.state;
 
-        formHandler = e => {
-            console.log(this.state[e.target.id]);
-            this.setState({ [e.target.id]: e.target.value });
-        };
-        submitHandler = () => {
-            sessionStorage.setItem('price', 4);
-            sessionStorage.setItem('coffee', 'black');
-            //this sends the information to the sign up api
-            const { location, email, password, retype } = this.state;
-
-            if (password === retype) {
-                axios
-                    .post('https://local-roasters-api.herokuapp.com/users/signup', {
-                        email: email,
-                        password: password,
-                        location: location,
-                        coffee: sessionStorage.getItem('coffee'),
-                        price: sessionStorage.getItem('price')
-                    })
-                    .then(res => localStorage.setItem('token', res.data.token))
-                    .then(() => this.props.history.push('/dashboard'))
-                    .catch(error => console.log(error));
-            } else {
-                alert('The passwords do not match');
-            }
-        };
-        render() {
-            return (
-                <div className='container-fluid'>
-                    <div className='jumbotron'>
-                        <h3>
-                            <i className='fas fa-mug-hot'></i> Local Roasters
-          </h3>
-                        <hr class='my-3'></hr>
-                        <p>Where are you getting your Coffee ? </p>
-                    </div>
-
-                    <div>
-                        <div className='form-group'>
-                            <input
-                                className='form-control'
-                                id='location'
-                                type='numeric'
-                                value={this.state.location}
-                                placeholder='zip code'
-                                onChange={event => this.formHandler(event)}
-                                required
-                            />
-                        </div>
-
-                        <div className='form-group'>
-                            <input
-                                className='form-control'
-                                id='email'
-                                type='text'
-                                value={this.state.email}
-                                placeholder='email'
-                                onChange={event => this.formHandler(event)}
-                                required
-                            />
-                        </div>
-                        <div className='form-group'>
-                            <input
-                                className='form-control'
-                                id='password'
-                                type='password'
-                                value={this.state.password}
-                                placeholder='password'
-                                onChange={event => this.formHandler(event)}
-                                required
-                            />
-                        </div>
-                        <div className='form-group'>
-                            <input
-                                className='form-control'
-                                id='retype'
-                                type='password'
-                                value={this.state.retype}
-                                placeholder='retype password'
-                                onChange={event => this.formHandler(event)}
-                                required
-                            />
-                        </div>
-                        <div className='form-group'>
-                            <input
-                                className='btn btn-lg btn-primary'
-                                type='submit'
-                                value='Continue'
-                                onClick={this.submitHandler}
-                            />
-                        </div>
-                    </div>
-                </div>
-            );
+        if (password === retype) {
+            axios
+                .post('https://local-roasters-api.herokuapp.com/users/signup', {
+                    email: email,
+                    password: password,
+                    location: location,
+                    coffee: sessionStorage.getItem('coffee'),
+                    price: sessionStorage.getItem('price')
+                })
+                .then(res => localStorage.setItem('token', res.data.token))
+                .then(() => this.props.history.push('/dashboard'))
+                .catch(error => console.log(error));
+        } else {
+            alert('The passwords do not match');
         }
     }
+
+    render() {
+        return (
+            <div className='container-fluid'>
+                <div className='jumbotron'>
+                    <h3>
+                        <i className='fas fa-mug-hot'></i> Local Roasters
+          </h3>
+                    <hr class='my-3'></hr>
+                    <p>Where are you getting your Coffee ? </p>
+                </div>
+
+                <div>
+                    <div className='form-group'>
+                        <input
+                            className='form-control'
+                            id='location'
+                            type='numeric'
+                            value={this.state.location}
+                            placeholder='zip code'
+                            onChange={event => this.formHandler(event)}
+                            required
+                        />
+                    </div>
+
+                    <div className='form-group'>
+                        <input
+                            className='form-control'
+                            id='email'
+                            type='text'
+                            value={this.state.email}
+                            placeholder='email'
+                            onChange={event => this.formHandler(event)}
+                            required
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <input
+                            className='form-control'
+                            id='password'
+                            type='password'
+                            value={this.state.password}
+                            placeholder='password'
+                            onChange={event => this.formHandler(event)}
+                            required
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <input
+                            className='form-control'
+                            id='retype'
+                            type='password'
+                            value={this.state.retype}
+                            placeholder='retype password'
+                            onChange={event => this.formHandler(event)}
+                            required
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <input
+                            className='btn btn-lg btn-primary'
+                            type='submit'
+                            value='Continue'
+                            onClick={this.submitHandler}
+                        />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
