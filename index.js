68108229b2f59ba59841af056ebb06f495cbff4e
Mark Goldstein <markgoldstein12@gmail.com>
2019-09-28 20:06:33 -0400

    methods

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 connectDB()//Call and connect to the db
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept", "x-auth-token");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth-token");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
 
     next();
